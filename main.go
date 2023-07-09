f13e896cd4ad3d294915df755303444e132c8aae
Johan Kjölhede <gigurra@gmail.com>
2023-07-09 15:32:25 +0200

    wip

--- main.go
+++ main.go
@@ -11,22 +11,80 @@ import (
 const Version = "v0.0.1"
 
 var rootCmd = &cobra.Command{
-	Use:   "git-profile",
+	Use:   "git-profile [profile-name]",
 	Short: "Helper for managing multiple git profiles for the same user",
 	Long:  `Complete documentation is available at https://github.com/gigurra/flycd`,
-}
-
-var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List available profiles",
 	Run: func(cmd *cobra.Command, args []string) {
 
 		users := getAvailableProfiles()
 
-		fmt.Printf("Available profiles: \n")
-		for profileName, v := range users {
-			fmt.Printf(" - %s: %s, %s, %s\n", profileName, v.SSHConfig.IdentityFile, v.GitConfig.UserName, v.GitConfig.UserEmail)
+		if len(args) == 0 {
+
+			fmt.Printf("Available profiles: \n")
+			for profileName, v := range users {
+				fmt.Printf(" - %s: %s, %s, %s\n", profileName, v.SSHConfig.IdentityFile, v.GitConfig.UserName, v.GitConfig.UserEmail)
+			}
+		} else {
+
+			profileName := args[0]
+
+			users := getAvailableProfiles()
+
+			fmt.Printf("Setting profile '%s'\n", profileName)
+			profile, found := users[profileName]
+
+			if !found {
+				fmt.Printf("Error: profile '%s' not found\n", profileName)
+				os.Exit(1)
+			}
+
+			// set ssh config
+			fmt.Printf("Setting Git parameters\n")
+			fmt.Printf(" - git config --global user.name %s\n", profile.GitConfig.UserName)
+			err := exec.Command("git", "config", "--global", "user.name", profile.GitConfig.UserName).Run()
+			if err != nil {
+				fmt.Printf("Error setting git config user.name: %v\n", err)
+				os.Exit(1)
+			}
+
+			fmt.Printf(" - git config --global user.email %s\n", profile.GitConfig.UserEmail)
+			err = exec.Command("git", "config", "--global", "user.email", profile.GitConfig.UserEmail).Run()
+			if err != nil {
+				fmt.Printf("Error setting git config user.email: %v\n", err)
+				os.Exit(1)
+			}
+
+			// set ssh config
+			fmt.Printf("Setting SSH parameters\n")
+
+			newContents := fmt.Sprintf(
+				`Host *
+    User "git"
+    IdentityFile "%s"
+    IdentitiesOnly "yes"
+    IdentityAgent "%s"
+`,
+				profile.SSHConfig.IdentityFile,
+				profile.SSHConfig.IdentityAgent,
+			)
+
+			fmt.Printf("New ssh config:\n")
+			print(newContents)
+
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				fmt.Printf("Error getting home dir: %v\n", err)
+				os.Exit(1)
+			}
+
+			sshConfigFilePath := homeDir + "/.ssh/config"
+			err = os.WriteFile(sshConfigFilePath, []byte(newContents), 0644)
+			if err != nil {
+				fmt.Printf("Error writing %s: %v\n", sshConfigFilePath, err)
+				os.Exit(1)
+			}
 		}
+
 	},
 }
 
@@ -57,72 +115,6 @@ func getAvailableProfiles() Users {
 	return users
 }
 
-var setCmd = &cobra.Command{
-	Use:   "set <profile name>",
-	Short: "Set the git profile (ssh config and git config)",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-
-		profileName := args[0]
-
-		users := getAvailableProfiles()
-
-		fmt.Printf("Setting profile '%s'\n", profileName)
-		profile, found := users[profileName]
-
-		if !found {
-			fmt.Printf("Error: profile '%s' not found\n", profileName)
-			os.Exit(1)
-		}
-
-		// set ssh config
-		fmt.Printf("Setting Git parameters\n")
-		fmt.Printf(" - git config --global user.name %s\n", profile.GitConfig.UserName)
-		err := exec.Command("git", "config", "--global", "user.name", profile.GitConfig.UserName).Run()
-		if err != nil {
-			fmt.Printf("Error setting git config user.name: %v\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf(" - git config --global user.email %s\n", profile.GitConfig.UserEmail)
-		err = exec.Command("git", "config", "--global", "user.email", profile.GitConfig.UserEmail).Run()
-		if err != nil {
-			fmt.Printf("Error setting git config user.email: %v\n", err)
-			os.Exit(1)
-		}
-
-		// set ssh config
-		fmt.Printf("Setting SSH parameters\n")
-
-		newContents := fmt.Sprintf(
-			`Host *
-    User "git"
-    IdentityFile "%s"
-    IdentitiesOnly "yes"
-    IdentityAgent "%s"
-`,
-			profile.SSHConfig.IdentityFile,
-			profile.SSHConfig.IdentityAgent,
-		)
-
-		fmt.Printf("New ssh config:\n")
-		print(newContents)
-
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			fmt.Printf("Error getting home dir: %v\n", err)
-			os.Exit(1)
-		}
-
-		sshConfigFilePath := homeDir + "/.ssh/config"
-		err = os.WriteFile(sshConfigFilePath, []byte(newContents), 0644)
-		if err != nil {
-			fmt.Printf("Error writing %s: %v\n", sshConfigFilePath, err)
-			os.Exit(1)
-		}
-	},
-}
-
 func main() {
 
 	// Check that required applications are installed
@@ -134,9 +126,6 @@ func main() {
 			os.Exit(1)
 		}
 	}
-
-	// prepare cli
-	rootCmd.AddCommand(listCmd, setCmd)
 
 	// run cli
 	if err := rootCmd.Execute(); err != nil {
